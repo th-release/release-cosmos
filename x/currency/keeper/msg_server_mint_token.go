02d07d7cf2901430db0d5e3ee4425a0a4c0218aa
th-release <cth.release@gmail.com>
2025-07-19 04:48:31 +0900

    [update] currency mint

--- x/currency/keeper/msg_server_mint_token.go
+++ x/currency/keeper/msg_server_mint_token.go
@@ -2,10 +2,15 @@ package keeper
 
 import (
 	"context"
+	"errors"
 
 	"release/x/currency/types"
 
+	"cosmossdk.io/collections"
 	errorsmod "cosmossdk.io/errors"
+	"cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) MintToken(ctx context.Context, msg *types.MsgMintToken) (*types.MsgMintTokenResponse, error) {
@@ -13,7 +18,54 @@ func (k msgServer) MintToken(ctx context.Context, msg *types.MsgMintToken) (*typ
 		return nil, errorsmod.Wrap(err, "invalid authority address")
 	}
 
-	// TODO: Handle the message
+	val, err := k.Token.Get(ctx, msg.Denom)
+	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			return nil, errorsmod.Wrap(sdkerrors.ErrKeyNotFound, "Denom not found")
+		}
+
+		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, err.Error())
+	}
+
+	if msg.Owner != val.Owner {
+		return nil, errorsmod.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
+
+	if val.Supply+msg.Amount > val.TotalSupply {
+		return nil, errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "Cannot mint more than Max Supply")
+	}
+	moduleAcct := k.authKeeper.GetModuleAddress(types.ModuleName)
+
+	recipientAddress, err := sdk.AccAddressFromBech32(msg.To)
+	if err != nil {
+		return nil, err
+	}
+
+	var mintCoins sdk.Coins
+
+	mintCoins = mintCoins.Add(sdk.NewCoin(msg.Denom, math.NewInt(int64(msg.Amount))))
+	if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, mintCoins); err != nil {
+		return nil, err
+	}
+	if err := k.bankKeeper.SendCoins(ctx, moduleAcct, recipientAddress, mintCoins); err != nil {
+		return nil, err
+	}
+
+	var denom = types.Token{
+		Owner:        val.Owner,
+		Denom:        val.Denom,
+		Name:         val.Name,
+		Symbol:       val.Symbol,
+		MetadataUrl:  val.MetadataUrl,
+		TotalSupply:  val.TotalSupply,
+		Supply:       val.Supply,
+		Decimals:     val.Decimals,
+		InitialPrice: val.InitialPrice,
+	}
+
+	if err := k.Token.Set(ctx, denom.Denom, denom); err != nil {
+		return nil, errorsmod.Wrap(sdkerrors.ErrLogic, "failed to mint and send denom")
+	}
 
 	return &types.MsgMintTokenResponse{}, nil
 }
